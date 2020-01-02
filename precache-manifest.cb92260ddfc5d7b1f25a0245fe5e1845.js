09d54a7cca550bfb53ede39005154e94470e4be5
jahangir khan <khan03148@gmail.com>
2020-01-02 12:29:24 +0100

    Updates

---
+++ precache-manifest.cb92260ddfc5d7b1f25a0245fe5e1845.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ab0bf8116bf31b506adb1efacc6638c",
+    "url": "/Resturantapp/index.html"
+  },
+  {
+    "revision": "dd2a450df1b7c2c63abd",
+    "url": "/Resturantapp/static/css/2.86edcaff.chunk.css"
+  },
+  {
+    "revision": "5989123dce1c78f417c0",
+    "url": "/Resturantapp/static/css/main.2ebf73be.chunk.css"
+  },
+  {
+    "revision": "dd2a450df1b7c2c63abd",
+    "url": "/Resturantapp/static/js/2.27ba7067.chunk.js"
+  },
+  {
+    "revision": "5989123dce1c78f417c0",
+    "url": "/Resturantapp/static/js/main.cf7b859e.chunk.js"
+  },
+  {
+    "revision": "b85eee26aac3e6056558",
+    "url": "/Resturantapp/static/js/runtime-main.c71ac705.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Resturantapp/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Resturantapp/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Resturantapp/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Resturantapp/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Resturantapp/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
